45dbdf10c253e798ba6a0b22083e107f15d89640
AnkitaDubey <ankitadubey42004@gmail.com>
2025-09-21 22:26:52 +0530

    colour changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-green-600"></div>
+      <div
+        className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4"
+        style={{ borderColor: '#8F8578 transparent #8F8578 transparent' }}
+      ></div>
     </div>
   );
 };
 
-export default Loader; // ✅ Default export
+export default Loader;
